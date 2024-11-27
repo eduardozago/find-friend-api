65add675dba1d2af5ae8ce2f501d2e5a87eb5465
Eduardo Zago <zagoe22@gmail.com>
2024-11-27 16:10:28 -0300

    feat: implement create pet controller

--- src/http/controllers/pets/create.ts
+++ src/http/controllers/pets/create.ts
@@ -1,6 +1,28 @@
 import { MakeCreatePetUseCase } from '@/use-cases/factories/make-create-pet-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  const createBodySchema = z.object({
+    name: z.string(),
+    about: z.string(),
+    type: z.string(),
+    age: z.string(),
+    size: z.string(),
+  })
+
+  const { name, about, type, age, size } = createBodySchema.parse(request.body)
+
+  const createPet = MakeCreatePetUseCase()
+
+  await createPet.execute({
+    name,
+    about,
+    type,
+    age,
+    size,
+    orgId: request.user.sub,
+  })
+
+  return reply.status(201).send()
 }
