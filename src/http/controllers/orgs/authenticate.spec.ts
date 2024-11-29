f4a7c4dbfa99296dba1af0a69293edf3363a7201
Eduardo Zago <zagoe22@gmail.com>
2024-11-28 21:39:15 -0300

    test: add e2e test on orgs controllers

--- src/http/controllers/orgs/authenticate.spec.ts
+++ src/http/controllers/orgs/authenticate.spec.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, it, expect } from 'vitest'
 
-describe('Register (e2e)', () => {
+describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,8 +11,8 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
-    const response = await request(app.server).post('/orgs').send({
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/orgs').send({
       name: 'New ORG',
       email: 'org@example.com',
       password: '123456',
@@ -25,6 +25,14 @@ describe('Register (e2e)', () => {
       longitude: -89.6501,
     })
 
-    expect(response.statusCode).toEqual(201)
+    const response = await request(app.server).post('/sessions').send({
+      email: 'org@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
